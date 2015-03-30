644e0dd1577e59822f1d3d7f6be7122db7ba5a3a
moonlet <xiaorx@live.com>
2015-03-30 18:24:54 +0800

    添加 pep8 的插件

--- vim/setup.sh
+++ vim/setup.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+VIM_DIR=${HOME}/.vim
+VIM_FTPLUGIN_DIR=${VIM_DIR}/ftplugin
+VIM_PYTHON_DIR=${VIM_FTPLGIN_DIR}/python
+
+# 0. init
+mkdir -p ${VIM_DIR}
+mkdir -p ${VIM_FTPLUGIN_DIR}
+mkdir -p ${VIM_PYTHON_DIR}
+sudo apt-get install python-pip
 
 # 1. prepare
 sudo apt-get install git ctags curl vim
@@ -10,3 +19,7 @@ git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 # 3. get the vimrc file
 cp ~/.vimrc ~/.vimrc.bk
 curl https://raw.githubusercontent.com/xiaorx/vimrc/master/vim/vimrc > ~/.vimr
+
+# 4. setup pep8
+sudo pip install pep8
+wget http://www.vim.org/scripts/download_script.php?src_id=14366 -O ${VIM_PYTHON_DIR}/pep8.vim
