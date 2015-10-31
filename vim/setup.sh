91eeca0bf78be7c7c183810d349f4d09b91f10f2
moonlet <xiaorixin@gmail.com>
2015-10-31 21:45:54 +0800

    调整 vimrc 文件

--- vim/setup.sh
+++ vim/setup.sh
@@ -10,16 +10,11 @@ mkdir -p ${VIM_PYTHON_DIR}
 sudo apt-get install python-pip
 
 # 1. prepare
-sudo apt-get install git ctags curl vim
+sudo apt-get install git curl vim
 
 # 2. get all the resources
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-#git clone git://github.com/sigurdga/gnome-terminal-colors-solarized.git
 
 # 3. get the vimrc file
 cp ~/.vimrc ~/.vimrc.bk
 curl https://raw.githubusercontent.com/xiaorx/vimrc/master/vim/vimrc > ${HOME}/.vimrc
-
-# 4. setup pep8
-sudo pip install pep8
-wget http://www.vim.org/scripts/download_script.php?src_id=14366 -O ${VIM_PYTHON_DIR}/pep8.vim
